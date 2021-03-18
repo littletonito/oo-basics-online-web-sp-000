4ab966e41d567b71e1e87f6f4dd58a8dffafd8e1
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-18 01:49:14 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,4 +6,23 @@ class Shoe
     @brand = brand
   end
 
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color
+  end
+
+  def size=(size)
+    @size
+  end
+
+  def material=(material)
+    @material
+  end
+
+  def condition=(condition)
+    @condition
+  end
 end

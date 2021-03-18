134ec2d8f303b4c514ada55f635c33f1a21b25f6
Antonio Clavijo Cortes <littletonito@gmail.com>
2021-03-18 01:50:48 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -14,7 +14,15 @@ class Shoe
     @color
   end
 
+  def color
+    @color
+  end
+
   def size=(size)
+    @size
+  end
+
+  def size
     @size
   end
 
@@ -22,8 +30,16 @@ class Shoe
     @material
   end
 
+  def material
+    @material
+  end
+
   def condition=(condition)
     @condition
   end
-  
+
+  def condition
+    @condition
+  end
+
 end
